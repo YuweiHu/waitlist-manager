e4904584671f741d15d56f778579352b82aea944
CliffHu <ayowi@CliffHudeMacBook-Air.local>
2024-10-20 19:02:45 +0800

    fix connection type issue

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -1,38 +1,22 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import mongoose from "mongoose";
+import mongoose, { Connection } from "mongoose";
 
 const MONGODB_URI: string = "mongodb://localhost:27017/local";
 
-if (!MONGODB_URI) {
-  throw new Error(
-    "Please define the MONGODB_URI environment variable inside .env.local"
-  );
-}
+let cachedConnection: Connection | null = null;
 
-interface Cached {
-  conn: typeof mongoose | null;
-  promise: Promise<typeof mongoose> | null;
-}
-
-const cached: Cached = (global as any).mongoose || {
-  conn: null,
-  promise: null,
-};
-
-if (!cached) {
-  (global as any).mongoose = cached;
-}
-
-const dbConnect = async (): Promise<typeof mongoose> => {
-  if (cached.conn) {
-    return cached.conn;
+export default async function dbConnect() {
+  if (cachedConnection) {
+    console.log("Using cached db connection");
+    return cachedConnection;
   }
 
-  if (!cached.promise) {
-    cached.promise = mongoose.connect(MONGODB_URI).then((mongoose) => mongoose);
+  try {
+    const cnx = await mongoose.connect(MONGODB_URI);
+    cachedConnection = cnx.connection;
+    console.log("New mongodb connection established");
+    return cachedConnection;
+  } catch (error) {
+    console.log(error);
+    throw error;
   }
-  cached.conn = await cached.promise;
-  return cached.conn;
-};
-
-export default dbConnect;
+}
